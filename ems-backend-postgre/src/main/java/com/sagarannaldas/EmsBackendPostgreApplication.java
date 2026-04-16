46b9e7d2a8b074916bbf5ff78409ecd033078268
sagarannaldas <annal7057@gmail.com>
2026-04-16 06:12:03 +0530

    first commit

---
+++ ems-backend-postgre/src/main/java/com/sagarannaldas/EmsBackendPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.sagarannaldas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBackendPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBackendPostgreApplication.class, args);
+	}
+
+}
---
+++ ems-backend-postgre/src/test/java/com/sagarannaldas/EmsBackendPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarannaldas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
